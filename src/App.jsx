2057702e2b9e575c6f2bd3298b7ce6b32b8c8c64
Suman Ghimire <ghimiresuman116@gmail.com>
2023-11-02 22:00:53 +0545

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import AddProduct from './pages/Addproduct/AddProduct'
+import { ProductInfo } from './pages/product info/ProductInfo'
 
 function App() {
 
@@ -10,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Addproduct" element={<AddProduct/>} />
+        <Route path="/productInfo" element={<ProductInfo/> } />
+
       </Routes>
     </BrowserRouter>
   )
